7cbb83fdb07efd21aa39e752eb83685c53f86b34
Chris Markwich <cmarkwich@glgroup.com>
2016-01-27 23:13:32 -0500

    redesigned build of app without build folder

--- README.md
+++ README.md
@@ -19,17 +19,13 @@ Note: If you need to add more to the 'vendor' folder, make sure to specify the p
 ##### Development
 You will be working out of the 'assets' folder.
 
-Step 1: run 'gulp dev' to build everything for dev
+Step 1: run 'gulp dev' to run build, server and watches
 
-Step 2: run 'gulp server' (watches and rebuilds your sass and script files)
+Step 2: open 'localhost:3000' in your browser
 
-Step 3: open 'localhost:3000' in your browser
+step 3: begin coding
 
 ##### Production
-Production will be looking at the 'build' folder.
-
 Side Note: If you are hosting this in a subfolder on production, make sure to update the base url in 'views/preprocessed/layout.ejs'.
 
-Step 1: run 'gulp prod' to build everything
-
-Step 2: run 'npm start'
+Step 1: run 'npm start'
--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,7 @@ var paths = {
   templates: ['assets/templates/**/*'],
   sass: ['assets/sass/**/*'],
   view: ['views/preprocessed/layout.ejs'],
-  built: ['build/', 'views/layout.ejs']
+  built: ['views/layout.ejs']
 };
 
 function swallowError (error) {
@@ -40,79 +40,69 @@ function swallowError (error) {
 
 // The default task (call gulp, builds whole project)
 gulp.task('clean', function() {
-   return gulp.src(paths.built, {read: false})
-        .pipe(clean({force: true}));
+  return gulp.src(paths.built, {read: false})
+    .pipe(clean({force: true}));
 });
 
-gulp.task('process_dev', ['clean'], function() {
-    return gulp.src(paths.view)
+gulp.task('dev_process', ['clean'], function() {
+  return gulp.src(paths.view)
     .pipe(preprocess({context: { NODE_ENV: 'development'}}))
     .pipe(gulp.dest('views'))
 });
 
-gulp.task('process_prod', ['clean'], function() {
-    return gulp.src(paths.view)
+gulp.task('prod_process', ['clean'], function() {
+  return gulp.src(paths.view)
     .pipe(preprocess({context: { NODE_ENV: 'production'}}))
     .pipe(gulp.dest('views'))
 });
 
-gulp.task('scripts', ['clean'], function() {
-    return gulp.src(paths.scripts)
-      .pipe(gulpif(/[.]coffee$/, coffee()))
-      .pipe(concat('main.min.js'))
-      .pipe(ngmin())
-      .pipe(uglify({mangle: false}))
-      .pipe(gulp.dest('build/js'));
+gulp.task('prod_scripts', ['clean'], function() {
+  return gulp.src(paths.scripts)
+    .pipe(gulpif(/[.]coffee$/, coffee()))
+    .pipe(concat('main.min.js'))
+    .pipe(ngmin())
+    .pipe(uglify({mangle: false}))
+    .pipe(gulp.dest('assets/_compiled'));
 });
 
-gulp.task('sass', ['clean'], function () {
-    return gulp.src(paths.sass)
-        .pipe(sass())
-        .pipe(concat('main.min.css'))
-        .pipe(minifyCSS({keepBreaks:false}))
-        .pipe(gulp.dest('build/css'));
+gulp.task('prod_sass', ['clean'], function () {
+  return gulp.src(paths.sass)
+    .pipe(sass())
+    .pipe(concat('main.min.css'))
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('assets/_compiled'));
 });
 
-gulp.task('images', ['clean'], function() {
-  return gulp.src(paths.images)
-    .pipe(gulp.dest('build/img'));
+gulp.task('dev_sass', function () {
+  return gulp.src(paths.sass)
+    .pipe(sass())
+    .on('error', swallowError)
+    .pipe(concat('main.min.css'))
+    .pipe(minifyCSS({keepBreaks:false}))
+    .pipe(gulp.dest('assets/_compiled'));
 });
 
-gulp.task('fonts', ['clean'], function() {
-  return gulp.src(paths.fonts)
-    .pipe(gulp.dest('build/fonts'));
+gulp.task('dev_scripts', function() {
+  return gulp.src(paths.scripts)
+    .pipe(gulpif(/[.]coffee$/, coffee()))
+    .on('error', swallowError)
+    .pipe(concat('main.min.js'))
+    .pipe(gulp.dest('assets/_compiled'));
 });
 
-gulp.task('templates', ['clean'], function() {
-  return gulp.src(paths.templates)
-    .pipe(gulp.dest('build/templates'));
+gulp.task('dev_server', ['clean', 'dev_process', 'dev_scripts', 'dev_sass'], function () {
+  server.run({
+      file: 'server.js'
+  });
+  gulp.watch(paths.scripts, ['dev_scripts']);
+  gulp.watch(paths.sass, ['dev_sass']);
 });
 
-gulp.task('dev', ['clean', 'process_dev', 'devscripts', 'sass', 'images', 'fonts', 'templates']);
-gulp.task('prod', ['clean', 'process_prod', 'scripts', 'sass', 'images', 'fonts', 'templates']);
-
-// The dev task (call gulp server, builds scss and js files quickly while generating localhost)
-gulp.task('devsass', function () {
-    return gulp.src(paths.sass)
-        .pipe(sass())
-        .on('error', swallowError)
-        .pipe(concat('main.min.css'))
-        .pipe(minifyCSS({keepBreaks:false}))
-        .pipe(gulp.dest('build/css'));
+gulp.task('prod_server', ['clean', 'prod_process', 'prod_scripts', 'prod_sass'], function () {
+  server.run({
+      file: 'server.js'
+  });
 });
 
-gulp.task('devscripts', function() {
-    return gulp.src(paths.scripts)
-      .pipe(gulpif(/[.]coffee$/, coffee()))
-      .on('error', swallowError)
-      .pipe(concat('main.min.js'))
-      .pipe(gulp.dest('build/js'));
-});
-
-gulp.task('server', function () {
-    server.run({
-        file: 'server.js'
-    });
-  gulp.watch(paths.scripts, ['devscripts']);
-  gulp.watch(paths.sass, ['devsass']);
-});
+gulp.task('dev', ['dev_server']);
+gulp.task('prod', ['prod_server']);
